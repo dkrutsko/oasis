77f89e5ffada09e4dfb939075c369d7deff82079
David Krutsko <dave@krutsko.net>
2026-04-23 22:48:43 -0400

    Remove server

--- view/src/lib/types/timer.ts
+++
@@ -1,8 +0,0 @@
-//----------------------------------------------------------------------------//
-// Exports                                                                    //
-//----------------------------------------------------------------------------//
-
-////////////////////////////////////////////////////////////////////////////////
-/// Represents the return type of setTimeout for declaring timeout variables.
-
-export type TimerType = ReturnType<typeof setTimeout> | null;
